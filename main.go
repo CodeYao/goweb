38d830c5cac01adfcbe15f76c0f6261cbb661c8e
Codeyao <457875141@qq.com>
2018-07-03 13:59:56 +0800

    完成登录部分功能

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -16,8 +22,55 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Println(w, "Hello chenyao!")
+	fmt.Fprintf(w, "Hello chenyao!")
 }
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	//r.ParseForm() //解析参数
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+		t, _ := template.ParseFiles("web/login.html")
+		t.Execute(w, token)
+		//t.Execute(w, nil)
+	} else {
+		//请求的是登陆数据，那么执行登陆的逻辑判断
+		// fmt.Println("username:", r.Form["username"])
+		// fmt.Println("password:", r.Form["password"])
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			//验证token的合法性
+			fmt.Println(token)
+		} else {
+			//不存在token报错
+		}
+		// for k, v := range r.Form {
+		// 	fmt.Println("key:", k)
+		// 	fmt.Println("val:", strings.Join(v, ""))
+		// }
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //输出到服务器端
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username"))) //输出到客户端
+		// t, _ := template.ParseFiles("web/index.html")
+		// t.Execute(w, nil)
+	}
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
+	//fmt.Println(32 << 20)
 	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":50000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }

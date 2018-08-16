3fb9efd1ebb95e34ff7a4b4d40691b668a509209
Codeyao <457875141@qq.com>
2018-08-16 15:20:33 +0800

    log工具

--- test/readcerttest.go
+++ test/readcerttest.go
@@ -1,16 +1,10 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"国密算法/gmsm/sm2"
-)
-
-func main() {
-	cert, err := sm2.ReadCertificateFromPem("C:/Users/chenyao/Desktop/cert.pem")
-	if err != nil {
-		panic(err)
-	}
-	pageJson, _ := json.Marshal(cert)
-	fmt.Println(string(pageJson))
-}
+// func main() {
+// 	cert, err := sm2.ReadCertificateFromPem("C:/Users/chenyao/Desktop/cert.pem")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	pageJson, _ := json.Marshal(cert)
+// 	fmt.Println(string(pageJson))
+// }
